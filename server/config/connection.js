603fb657f7ce8885caf55b00f9f03ac5ab41078d
[Elias Montes] <[eliasmontes911@gmail.com]>
2023-10-02 18:29:59 -0700

    changed connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/book-search');
 
 module.exports = mongoose.connection;
